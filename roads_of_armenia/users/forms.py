c016f8f10073768760dec2668fedcb0d1d283a8d
Tigran958 <tigran.danielyan3@gmail.com>
2020-03-11 00:42:09 +0400

    new forms for Tour model and Login were created, and new USER_CHOICES choice- box was developed

--- roads_of_armenia/users/forms.py
+++ roads_of_armenia/users/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import User, Driver, Guide, Client, TourAgents
+from .models import User, Driver, Guide, Client, TourAgents, Tour
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.forms import inlineformset_factory
 from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 
+USER_CHOICES = (
+    (1, _("Driver")),
+    (2, _("Client")),
+    (3, _("Guide")),
+    (4, _("TourAgents"))
+)
 
 class CustomUserForm(UserCreationForm):
 
@@ -63,3 +70,16 @@ CollectionTitleFormSetTourAgents = inlineformset_factory(
     User, TourAgents, form=TourAgentsUserCreationForm,
     fields='__all__', extra=1, can_delete=True
 )
+
+class TourCreationForm(ModelForm):
+    class Meta:
+        model = Tour
+        fields = '__all__'  
+  
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username',]
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    user_choices = forms.ChoiceField(choices = USER_CHOICES, required=True)
